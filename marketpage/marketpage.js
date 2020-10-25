3d24a1718210ce288d8bc49e12545aa8e9e748dc
Michel <michelbehnia2003@gmail.com>
2020-10-25 17:04:05 -0400

    organizing the code for marketpage

--- marketpage/marketpage.js
+++ marketpage/marketpage.js
@@ -1,17 +1,41 @@
-function fetchStockInfo(){
-    let apiKey = "S7654FSF3ELETV8H";
-    let apiCall = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=${apiKey}`;
+function displayStockInfo(){
+    let userInput = document.getElementById('userInputBox').value;
+    let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
+    let apiCall =`https://financialmodelingprep.com/api/v3/quote/${userInput}?apikey=${apiKey}`;
 
-    fetch(apiCall)
+    if(userInput !== '' && userInput.length <= 5){
+
+        fetch(apiCall)
         .then(
             function(response){
-                return response.json();
-            }
-        )
-        .then(
+                if(response){
+                    return response.json();
+                }
+                throw Error('Error!');
+            })
+            .then(
             function(data){
                 console.log(data);
-                document.getElementById("Info").innerHTML=data;
-            }
-        )
+                const {name , price , change , priceAvg200, dayHigh , dayLow} = data[0]
+
+                document.getElementById("companyName").innerHTML = name;
+
+                document.getElementById("stockPrice").innerHTML = '$' + price;
+
+                document.getElementById("stockPriceChange").innerHTML = '$' + change;
+
+                document.getElementById("stockPriceAvg200").innerHTML = 'Average Price for past 200 exchanged: $' + priceAvg200;
+
+                document.getElementById("stockDayHigh").innerHTML = 'Todays high: $' + dayHigh;
+
+                document.getElementById("stockDayLow").innerHTML = 'Todays low: $' + dayLow;
+            })
+            .catch(
+            function(error){
+                console.log(error);
+            })
+
+    } else {
+        document.getElementById("stockInformation").innerHTML = 'enter valid input';
+    }
 }
