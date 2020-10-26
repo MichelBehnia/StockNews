394ee5978eeb3473f07521c3fa4ebeb006d001a2
TonyZhou <ztony1414@gmail.com>
2020-10-25 20:36:36 -0400

    Added api interaction

--- marketpage/marketpage.js
+++ marketpage/marketpage.js
@@ -39,3 +39,63 @@ function displayStockInfo(){
         document.getElementById("stockInformation").innerHTML = 'enter valid input';
     }
 }
+
+
+function fetchStockInfo() {
+    let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
+    let apiCall = `https://financialmodelingprep.com/api/v3/quote/MSFT?apikey=${apiKey}`;
+
+
+    fetch(apiCall)
+      .then(
+        function (response) {
+          return response.json();
+        }
+      )
+      .then(
+        function(data){
+            console.log(data);
+            const {name , price , change , priceAvg200, dayHigh , dayLow} = data[0]
+
+            document.getElementById("companyName").innerHTML = name;
+
+            document.getElementById("stockPrice").innerHTML = '$' + price;
+
+            document.getElementById("stockPriceChange").innerHTML = '$' + change;
+
+            document.getElementById("stockPriceAvg200").innerHTML = 'Average Price for past 200 exchanged: $' + priceAvg200;
+
+            document.getElementById("stockDayHigh").innerHTML = 'Todays high: $' + dayHigh;
+
+            document.getElementById("stockDayLow").innerHTML = 'Todays low: $' + dayLow;
+        })
+  }
+
+  function fetchMutualFunds() {
+    let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
+    let apiCall = `  https://financialmodelingprep.com/api/v3/quote/VFIAX?apikey=${apiKey}`;
+
+    fetch(apiCall)
+      .then(
+        function (response) {
+          return response.json();
+        }
+      )
+      .then(
+        function(data){
+            console.log(data);
+            const {name , price , change , priceAvg50, priceAvg200 , changesPercentage} = data[0]
+
+            document.getElementById("companyName").innerHTML = name;
+
+            document.getElementById("stockPrice").innerHTML = '$' + price;
+
+            document.getElementById("stockPriceChange").innerHTML = '$' + change;
+
+            document.getElementById("stockPriceAvg50").innerHTML = 'Average Price for past 50 exchanged: $' + priceAvg50;
+
+            document.getElementById("stockPriceAvg200").innerHTML =  'Average Price for past 200 exchanged: $' + priceAvg200;
+
+            document.getElementById("changePercentage").innerHTML = 'Change percentage: ' + changesPercentage;
+        })
+  }
