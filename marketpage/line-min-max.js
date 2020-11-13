5bb19ef247a8b8ab812998324689674578e0a8f1
TonyZhou <ztony1414@gmail.com>
2020-11-12 20:13:31 -0500

    100 done

--- marketpage/line-min-max.js
+++ marketpage/line-min-max.js
@@ -220,296 +220,370 @@ $(document).ready(function () {
 			});
 })
 function fetchGraph(symbol) {
-	
+
 	let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
 	let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?apikey=${apiKey}`;
 	fetch(apiCall)
-	.then(
-		function (response) {
-			return response.json();
-		}
-	)
-	.then(
-		function (data) {
-			console.log(data);
-			console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
-			console.log("array size=" + data['historical'].length);
-			iDataSize = data['historical'].length;
-dateArrayfetch.length = 0;
-priceArrayfetch.length = 0;
-
-
-			for (i = 0; i < 31; i++) {
-				mydatefetch = data['historical'][i]['date'];
-				openPricefetch = data['historical'][i]['open'];
-				dateArrayfetch.push(mydatefetch);
-				priceArrayfetch.push(openPricefetch);
+		.then(
+			function (response) {
+				return response.json();
 			}
-			dateArrayfetch.reverse();
-			priceArrayfetch.reverse();
-			//get canvas
-			var ctx = $("#line-chartcanvasfetch");
+		)
+		.then(
+			function (data) {
+				console.log(data);
+				console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
+				console.log("array size=" + data['historical'].length);
+				iDataSize = data['historical'].length;
+				dateArrayfetch.length = 0;
+				priceArrayfetch.length = 0;
 
-			var data = {
-				labels: [],
-				datasets: [
-					{
-						label: "Price($)",
-						data: [10, 50, 25, 70, 40],
-						backgroundColor: "blue",
-						borderColor: "lightblue",
-						fill: false,
-						lineTension: 0,
-						pointRadius: 5
-					}
 
-				]
-			};
-			data.labels = dateArrayfetch;
-			data.datasets[0].data = priceArrayfetch;
+				for (i = 0; i < 31; i++) {
+					mydatefetch = data['historical'][i]['date'];
+					openPricefetch = data['historical'][i]['open'];
+					dateArrayfetch.push(mydatefetch);
+					priceArrayfetch.push(openPricefetch);
+				}
+				dateArrayfetch.reverse();
+				priceArrayfetch.reverse();
+				//get canvas
+				var ctx = $("#line-chartcanvasfetch");
 
-			var options = {
-				title: {
-					display: true,
-					position: "top",
-					text: symbol + " Historical Price",
-					fontSize: 18,
-					fontColor: "#111"
-				},
-				legend: {
-					display: true,
-					position: "bottom"
-				},
+				var data = {
+					labels: [],
+					datasets: [
+						{
+							label: "Price($)",
+							data: [10, 50, 25, 70, 40],
+							backgroundColor: "blue",
+							borderColor: "lightblue",
+							fill: false,
+							lineTension: 0,
+							pointRadius: 5
+						}
 
-			};
+					]
+				};
+				data.labels = dateArrayfetch;
+				data.datasets[0].data = priceArrayfetch;
 
-			var chart = new Chart(ctx, {
-				type: "line",
-				data: data,
-				options: options
+				var options = {
+					title: {
+						display: true,
+						position: "top",
+						text: symbol + " Historical Price",
+						fontSize: 18,
+						fontColor: "#111"
+					},
+					legend: {
+						display: true,
+						position: "bottom"
+					},
+
+				};
+
+				var chart = new Chart(ctx, {
+					type: "line",
+					data: data,
+					options: options
+				});
+
 			});
 
-		});
+}
 
-	}
+function searchGraph() {
+	let userInput = document.getElementById('userInputBox').value;
+	let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
+	let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${userInput}?apikey=${apiKey}`;
+	fetch(apiCall)
+		.then(
+			function (response) {
+				return response.json();
+			}
+		)
+		.then(
+			function (data) {
+				console.log(data);
+				console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
+				console.log("array size=" + data['historical'].length);
+				iDataSize = data['historical'].length;
+				dateArrayfetch.length = 0;
+				priceArrayfetch.length = 0;
 
-	function hoverMiddle(symbol) {
-		let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
-		let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?apikey=${apiKey}`;
-	
-		fetch(apiCall)
-			.then(
-				function (response) {
-					return response.json();
+
+				for (i = 0; i < 31; i++) {
+					mydatefetch = data['historical'][i]['date'];
+					openPricefetch = data['historical'][i]['open'];
+					dateArrayfetch.push(mydatefetch);
+					priceArrayfetch.push(openPricefetch);
 				}
-			)
-			.then(
-				function (data) {
-					console.log(data);
-					console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
-					console.log("array size=" + data['historical'].length);
-					iDataSize = data['historical'].length;
-					dateArray.length = 0;
-priceArray.length = 0;
-	
-	
-	
-					for (i = 0; i < 31; i++) {
-						mydate = data['historical'][i]['date'];
-						openPrice = data['historical'][i]['open'];
-						dateArray.push(mydate);
-						priceArray.push(openPrice);
-					}
-					dateArray.reverse();
-					priceArray.reverse();
-					//get canvas
-					var ctx = $("#line-chartcanvas");
-	
-					var data = {
-						labels: [],
-						datasets: [
-							{
-								label: "Price($)",
-								data: [10, 50, 25, 70, 40],
-								backgroundColor: "blue",
-								borderColor: "lightblue",
-								fill: false,
-								lineTension: 0,
-								pointRadius: 5
-							}
-	
-						]
-					};
-					data.labels = dateArray;
-					data.datasets[0].data = priceArray;
-	
-					var options = {
-						title: {
-							display: true,
-							position: "top",
-							text: symbol + " Historical Price",
-							fontSize: 18,
-							fontColor: "#111"
-						},
-						legend: {
-							display: true,
-							position: "bottom"
-						},
-	
-					};
-	
-					var chart = new Chart(ctx, {
-						type: "line",
-						data: data,
-						options: options
-					});
-	
-				});
-	}
+				dateArrayfetch.reverse();
+				priceArrayfetch.reverse();
+				//get canvas
+				var ctx = $("#line-chartcanvasfetch");
 
-	function hoverLeft(symbol) {
-		let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
-		let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?apikey=${apiKey}`;
-		fetch(apiCall)
-			.then(
-				function (response) {
-					return response.json();
-				}
-			)
-			.then(
-				function (data) {
-					console.log(data);
-					console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
-					console.log("array size=" + data['historical'].length);
-					iDataSize = data['historical'].length;
-					dateArray2.length = 0;
-					priceArray2.length = 0;
-	
-	
-	
-					for (i = 0; i < 31; i++) {
-						mydate2 = data['historical'][i]['date'];
-						openPrice2 = data['historical'][i]['open'];
-						dateArray2.push(mydate2);
-						priceArray2.push(openPrice2);
-					}
-					dateArray2.reverse();
-					priceArray2.reverse();
-					//get canvas
-					var ctx = $("#line-chartcanvas2");
-	
-					var data = {
-						labels: [],
-						datasets: [
-							{
-								label: "Price($)",
-								data: [10, 50, 25, 70, 40],
-								backgroundColor: "blue",
-								borderColor: "lightblue",
-								fill: false,
-								lineTension: 0,
-								pointRadius: 5
-							}
-	
-						]
-					};
-					data.labels = dateArray2;
-					data.datasets[0].data = priceArray2;
-	
-					var options = {
-						title: {
-							display: true,
-							position: "top",
-							text: symbol + " Historical Price",
-							fontSize: 18,
-							fontColor: "#111"
-						},
-						legend: {
-							display: true,
-							position: "bottom"
-						},
-	
-					};
-	
-					var chart = new Chart(ctx, {
-						type: "line",
-						data: data,
-						options: options
-					});
-	
+				var data = {
+					labels: [],
+					datasets: [
+						{
+							label: "Price($)",
+							data: [10, 50, 25, 70, 40],
+							backgroundColor: "blue",
+							borderColor: "lightblue",
+							fill: false,
+							lineTension: 0,
+							pointRadius: 5
+						}
+
+					]
+				};
+				data.labels = dateArrayfetch;
+				data.datasets[0].data = priceArrayfetch;
+
+				var options = {
+					title: {
+						display: true,
+						position: "top",
+						text: userInput + " Historical Price",
+						fontSize: 18,
+						fontColor: "#111"
+					},
+					legend: {
+						display: true,
+						position: "bottom"
+					},
+
+				};
+
+				var chart = new Chart(ctx, {
+					type: "line",
+					data: data,
+					options: options
 				});
-	}
+
+			});
+
+}
+
+function hoverMiddle(symbol) {
+	let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
+	let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?apikey=${apiKey}`;
+
+	fetch(apiCall)
+		.then(
+			function (response) {
+				return response.json();
+			}
+		)
+		.then(
+			function (data) {
+				console.log(data);
+				console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
+				console.log("array size=" + data['historical'].length);
+				iDataSize = data['historical'].length;
+				dateArray.length = 0;
+				priceArray.length = 0;
+
+
+
+				for (i = 0; i < 31; i++) {
+					mydate = data['historical'][i]['date'];
+					openPrice = data['historical'][i]['open'];
+					dateArray.push(mydate);
+					priceArray.push(openPrice);
+				}
+				dateArray.reverse();
+				priceArray.reverse();
+				//get canvas
+				var ctx = $("#line-chartcanvas");
+
+				var data = {
+					labels: [],
+					datasets: [
+						{
+							label: "Price($)",
+							data: [10, 50, 25, 70, 40],
+							backgroundColor: "blue",
+							borderColor: "lightblue",
+							fill: false,
+							lineTension: 0,
+							pointRadius: 5
+						}
+
+					]
+				};
+				data.labels = dateArray;
+				data.datasets[0].data = priceArray;
+
+				var options = {
+					title: {
+						display: true,
+						position: "top",
+						text: symbol + " Historical Price",
+						fontSize: 18,
+						fontColor: "#111"
+					},
+					legend: {
+						display: true,
+						position: "bottom"
+					},
+
+				};
+
+				var chart = new Chart(ctx, {
+					type: "line",
+					data: data,
+					options: options
+				});
+
+			});
+}
+
+function hoverLeft(symbol) {
+	let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
+	let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?apikey=${apiKey}`;
+	fetch(apiCall)
+		.then(
+			function (response) {
+				return response.json();
+			}
+		)
+		.then(
+			function (data) {
+				console.log(data);
+				console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
+				console.log("array size=" + data['historical'].length);
+				iDataSize = data['historical'].length;
+				dateArray2.length = 0;
+				priceArray2.length = 0;
+
+
+
+				for (i = 0; i < 31; i++) {
+					mydate2 = data['historical'][i]['date'];
+					openPrice2 = data['historical'][i]['open'];
+					dateArray2.push(mydate2);
+					priceArray2.push(openPrice2);
+				}
+				dateArray2.reverse();
+				priceArray2.reverse();
+				//get canvas
+				var ctx = $("#line-chartcanvas2");
+
+				var data = {
+					labels: [],
+					datasets: [
+						{
+							label: "Price($)",
+							data: [10, 50, 25, 70, 40],
+							backgroundColor: "blue",
+							borderColor: "lightblue",
+							fill: false,
+							lineTension: 0,
+							pointRadius: 5
+						}
+
+					]
+				};
+				data.labels = dateArray2;
+				data.datasets[0].data = priceArray2;
+
+				var options = {
+					title: {
+						display: true,
+						position: "top",
+						text: symbol + " Historical Price",
+						fontSize: 18,
+						fontColor: "#111"
+					},
+					legend: {
+						display: true,
+						position: "bottom"
+					},
+
+				};
+
+				var chart = new Chart(ctx, {
+					type: "line",
+					data: data,
+					options: options
+				});
+
+			});
+}
 
 function hoverRight(symbol) {
-		let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
-		let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?apikey=${apiKey}`;
-		fetch(apiCall)
-			.then(
-				function (response) {
-					return response.json();
+	let apiKey = '2cc11b6f558b90e7e81d410711573cc9';
+	let apiCall = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?apikey=${apiKey}`;
+	fetch(apiCall)
+		.then(
+			function (response) {
+				return response.json();
+			}
+		)
+		.then(
+			function (data) {
+				console.log(data);
+				console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
+				console.log("array size=" + data['historical'].length);
+				iDataSize = data['historical'].length;
+				dateArray3.length = 0;
+				priceArray3.length = 0;
+
+
+
+
+				for (i = 0; i < 31; i++) {
+					mydate3 = data['historical'][i]['date'];
+					openPrice3 = data['historical'][i]['open'];
+					dateArray3.push(mydate3);
+					priceArray3.push(openPrice3);
 				}
-			)
-			.then(
-				function (data) {
-					console.log(data);
-					console.log("date=" + data['historical'][0]['date'] + " open=" + data['historical'][0]['open']);
-					console.log("array size=" + data['historical'].length);
-					iDataSize = data['historical'].length;
-					dateArray3.length = 0;
-					priceArray3.length = 0;
-	
-	
-	
-	
-					for (i = 0; i < 31; i++) {
-						mydate3 = data['historical'][i]['date'];
-						openPrice3 = data['historical'][i]['open'];
-						dateArray3.push(mydate3);
-						priceArray3.push(openPrice3);
-					}
-					dateArray3.reverse();
-					priceArray3.reverse();
-					//get canvas
-					var ctx = $("#line-chartcanvas3");
-	
-					var data = {
-						labels: [],
-						datasets: [
-							{
-								label: "Price($)",
-								data: [10, 50, 25, 70, 40],
-								backgroundColor: "blue",
-								borderColor: "lightblue",
-								fill: false,
-								lineTension: 0,
-								pointRadius: 5
-							}
-	
-						]
-					};
-					data.labels = dateArray3;
-					data.datasets[0].data = priceArray3;
-	
-					var options = {
-						title: {
-							display: true,
-							position: "top",
-							text: symbol + " Historical Price",
-							fontSize: 18,
-							fontColor: "#111"
-						},
-						legend: {
-							display: true,
-							position: "bottom"
-						},
-	
-					};
-	
-					var chart = new Chart(ctx, {
-						type: "line",
-						data: data,
-						options: options
-					});
-	
+				dateArray3.reverse();
+				priceArray3.reverse();
+				//get canvas
+				var ctx = $("#line-chartcanvas3");
+
+				var data = {
+					labels: [],
+					datasets: [
+						{
+							label: "Price($)",
+							data: [10, 50, 25, 70, 40],
+							backgroundColor: "blue",
+							borderColor: "lightblue",
+							fill: false,
+							lineTension: 0,
+							pointRadius: 5
+						}
+
+					]
+				};
+				data.labels = dateArray3;
+				data.datasets[0].data = priceArray3;
+
+				var options = {
+					title: {
+						display: true,
+						position: "top",
+						text: symbol + " Historical Price",
+						fontSize: 18,
+						fontColor: "#111"
+					},
+					legend: {
+						display: true,
+						position: "bottom"
+					},
+
+				};
+
+				var chart = new Chart(ctx, {
+					type: "line",
+					data: data,
+					options: options
 				});
-	}
+
+			});
+}
